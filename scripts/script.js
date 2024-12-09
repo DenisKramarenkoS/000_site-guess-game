24653f7ff34e600e8dd73b1493fec968f3d11da8
Denis Kramarenko <cashflow.dkm@gmail.com>
2024-12-09 22:16:16 +1000

    UPD: new features

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
-const randomValue =
-  Math.floor(Math.random() * 100) === 0 ? 1 : Math.floor(Math.random() * 100); // Random anchor value from 1 to 100
+let randomValue;
 const sendButton = document.getElementById("sendButton");
 const playAgainButton = document.getElementById("playAgainButton");
 const guesses = document.getElementById("guesses");
@@ -15,18 +14,27 @@ const curtain = document.getElementById("curtain");
 const buttonForm = document.getElementsByClassName("button-form");
 const textDullColor = document.getElementsByClassName("textDullColor");
 const menuButton = document.getElementsByClassName("menuButton");
+const hardModeButton = document.getElementById("hardModeButton");
+const insaneModeButton = document.getElementById("insaneModeButton");
+const info = document.getElementById("info");
+const numbersBlock = document.getElementById("numbersBlock");
+const numberButton = document.getElementsByClassName("numberButton");
 
-let lowValue = 0;
-let highValue = 100;
+let maxValue;
+
+let lowValue;
+let highValue;
 
 let checkClickAnswers = true;
 let isGameWon = false;
 let isCustomInputActive = false;
 let isControlPressed = false;
+let isValueCreated = false;
+let isHardModeEnable = false;
+let isInsaneModeEnable = false;
 
 let countAttempt = 0;
-
-console.log(randomValue); // Checking in Console
+let maxCountAttempts;
 
 formSubmit.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -76,19 +84,40 @@ guessField.addEventListener("blur", () => {
 
 // Send button
 sendButton.addEventListener("click", function () {
+  if (!isValueCreated) {
+    if (isHardModeEnable) {
+      maxValue = 1000;
+      maxCountAttempts = 10;
+    } else if (isInsaneModeEnable) {
+      maxValue = 4000000000;
+      maxCountAttempts = 32;
+    } else {
+      maxValue = 100;
+      maxCountAttempts = 7;
+    }
+    randomValue = Math.floor(Math.random() * maxValue);
+
+    console.log(randomValue);
+
+    isValueCreated = true;
+    lowValue = 1;
+    highValue = maxValue;
+  }
+
   const guessValue = Number(guessField.value); // User input
-  const russianVersionCheck = document.body.classList.contains("russianVersion") 
+  const russianVersionCheck =
+    document.body.classList.contains("russianVersion");
 
   if (checkClickAnswers) {
     // If SendButton was click show the addict content
-    for (let i = 0; i < displayedBlock.length; i += 1) {
+    for (let i = 0; i < displayedBlock.length; i++) {
       displayedBlock[i].style.display = "block";
     }
     answer.style.display = "flex";
     checkClickAnswers = false; // Show once
   }
 
-  if (guessValue < 1 || guessValue > 100) {
+  if (guessValue < 1 || guessValue > maxValue) {
     // If Guess Value out of range doing this
 
     if (russianVersionCheck) {
@@ -98,40 +127,60 @@ sendButton.addEventListener("click", function () {
     }
 
     textAnswer.textContent = "Incorrect Value";
-    answer.style.backgroundColor = "yellow";
-    answer.style.borderColor = "darkorange";
+
+    answer.style.color = "hsl(25 100% 50%)";
+    answer.style.borderColor = "hsl(25 100% 50%)";
   } else {
-    countAttempt += 1;
-    attemptNumber.textContent -= 1;
+    countAttempt++;
+    attemptNumber.textContent = maxCountAttempts - countAttempt;
     guesses.textContent += " " + guessValue; // Adding a value in the Previous panel
     if (randomValue !== guessValue) {
-      if (russianVersionCheck) textAnswer.textContent = "Неправильно"
+      if (russianVersionCheck) textAnswer.textContent = "Неправильно";
       else textAnswer.textContent = "Wrong";
-      answer.style.backgroundColor = "red";
-      answer.style.borderColor = "darkred";
+      answer.style.color = "red";
+      answer.style.borderColor = "red";
 
       if (randomValue > guessValue) {
         if (guessValue > lowValue) lowValue = guessValue;
 
-        hint.textContent = `Too low, try numbers in the range (${lowValue} - ${highValue})`;
+        if (russianVersionCheck) {
+          hint.textContent = `Слишком мало, попробуй в диапазоне (${lowValue} - ${highValue})`;
+        } else {
+          hint.textContent = `Too low, try numbers in the range (${lowValue} - ${highValue})`;
+        }
       } else if (randomValue < guessValue) {
         if (guessValue < highValue) highValue = guessValue;
 
-        hint.textContent = `Too big, try numbers in the range (${lowValue} - ${highValue})`;
+        if (russianVersionCheck) {
+          hint.textContent = `Слишком много, попробуй в диапазоне (${lowValue} - ${highValue})`;
+        } else {
+          hint.textContent = `Too bit, try numbers in the range (${lowValue} - ${highValue})`;
+        }
       }
 
-      if (countAttempt >= 7) {
-        hint.textContent = "Your attempts is over. It's was the " + randomValue;
+      if (countAttempt >= maxCountAttempts) {
+        if (russianVersionCheck) {
+          hint.textContent =
+            "Попытки закончились. Это было число " + randomValue;
+        } else {
+          hint.textContent =
+            "Your attempts is over. It's was the " + randomValue;
+        }
         guessField.setAttribute("readonly", "true");
         sendButton.setAttribute("disabled", "true");
         isGameWon = true;
       }
     } else {
-      textAnswer.textContent = "Correct";
-      answer.style.backgroundColor = "greenyellow";
-      answer.style.borderColor = "green";
+      if (russianVersionCheck) {
+        textAnswer.textContent = "Верно";
+        hint.textContent = "Наши поздравления!";
+      } else {
+        textAnswer.textContent = "Correct";
+        hint.textContent = "Your answer is correct! Congratulations!";
+      }
 
-      hint.textContent = "Your answer is correct! Congratulations!";
+      answer.style.color = "green";
+      answer.style.borderColor = "green";
 
       guessField.setAttribute("readonly", "true");
       sendButton.setAttribute("disabled", "true");
@@ -142,48 +191,126 @@ sendButton.addEventListener("click", function () {
 });
 
 switchTheme.addEventListener("click", () => {
-  if (document.body.classList.contains("lightTheme")) {
-    document.body.classList.replace("lightTheme", "blackTheme");
+  if (document.body.classList.contains("blackTheme")) {
+    document.body.classList.replace("blackTheme", "whiteTheme");
 
     // menu buttons
-    for (let i = 0; i < menuButton.length; i += 1) {
-      menuButton[i].style.color = "white";
-      menuButton[i].style.backgroundColor = "black";
+    for (let i = 0; i < menuButton.length; i++) {
+      if (menuButton[i].classList.contains("modeButton")) {
+        if (menuButton[i].id === "hardModeButton") {
+          menuButton[i].style.backgroundColor = "orange";
+          menuButton[i].style.border = "none";
+        } else {
+          menuButton[i].style.backgroundColor = "red";
+          menuButton[i].style.border = "none";
+        }
+        menuButton[i].style.color = "black";
+      } else {
+        menuButton[i].style.color = "white";
+        menuButton[i].style.backgroundColor = "black";
+      }
     }
 
     // banner, which close info-text
     curtain.style.backgroundColor = "white";
 
     // buttons
-    for (let i = 0; i < buttonForm.length; i += 1) {
+    for (let i = 0; i < buttonForm.length; i++) {
       buttonForm[i].classList.add("black-form");
     }
 
     // Text with dull color
-    for (let i = 0; i < textDullColor.length; i += 1) {
+    for (let i = 0; i < textDullColor.length; i++) {
       textDullColor[i].style.color = "hsl(0 0% 15%)";
     }
+
+    // numbers buttons
+    for (let i = 0; i < numberButton.length; i++) {
+      numberButton[i].classList.add("dullWhiteTheme");
+    }
   } else {
-    document.body.classList.replace("blackTheme", "lightTheme");
+    document.body.classList.replace("whiteTheme", "blackTheme");
 
     // menu buttons
-    for (let i = 0; i < menuButton.length; i += 1) {
-      menuButton[i].style.color = "black";
-      menuButton[i].style.backgroundColor = "white";
+    for (let i = 0; i < menuButton.length; i++) {
+      if (menuButton[i].classList.contains("modeButton")) {
+        if (menuButton[i].id === "hardModeButton") {
+          menuButton[i].style.borderColor = "orange";
+          menuButton[i].style.color = "orange";
+          menuButton[i].style.border = "3px solid hsl(25 100% 50%)";
+        } else {
+          menuButton[i].style.borderColor = "red";
+          menuButton[i].style.color = "red";
+          menuButton[i].style.border = "3px solid red";
+        }
+        menuButton[i].style.backgroundColor = "black";
+      } else {
+        menuButton[i].style.color = "black";
+        menuButton[i].style.backgroundColor = "white";
+      }
     }
 
     // banner, which close info-text
     curtain.style.backgroundColor = "black";
 
     //buttons
-    for (let i = 0; i < buttonForm.length; i += 1) {
+    for (let i = 0; i < buttonForm.length; i++) {
       buttonForm[i].classList.remove("black-form");
     }
 
     // Text with dull color
-    for (let i = 0; i < textDullColor.length; i += 1) {
+    for (let i = 0; i < textDullColor.length; i++) {
       textDullColor[i].style.color = "hsl(0 0% 85%)";
     }
+
+    // numbers buttons
+    for (let i = 0; i < numberButton.length; i++) {
+      numberButton[i].classList.remove("dullWhiteTheme");
+    }
+  }
+});
+
+hardModeButton.addEventListener("click", function () {
+  isHardModeEnable = true;
+  const russianVersionCheck =
+    document.body.classList.contains("russianVersion");
+
+  if (russianVersionCheck) {
+    info.textContent =
+      "Мы загадали число от 1 до 1000. Сможешь отгадать его за 10 попыток?";
+  } else {
+    info.textContent =
+      "We have selected a number from 1 to 1000. See if you can guess it in 10 turns or fewer";
+  }
+});
+
+insaneModeButton.addEventListener("click", function () {
+  isInsaneModeEnable = true;
+  const russianVersionCheck =
+    document.body.classList.contains("russianVersion");
+
+  if (russianVersionCheck) {
+    info.textContent =
+      "Мы загадали число от 1 до 4 миллиардов. Сможешь отгадать его за 32 попытки?";
+  } else {
+    info.textContent =
+      "We have selected a number from 1 to 4 billion. See if you can guess it in 32 turns or fewer";
+  }
+});
+
+numbersBlock.addEventListener("click", function (event) {
+  const target = event.target;
+
+  if (
+    target.tagName === "BUTTON" &&
+    target.id !== "clearOneNumber" &&
+    target.id !== "clearAllNumbers"
+  ) {
+    guessField.value += Number(target.textContent);
+  } else if (target.id === "clearAllNumbers") {
+    guessField.value = "";
+  } else if (target.id === "clearOneNumber") {
+    guessField.value = guessField.value.slice(0, -1);
   }
 });
 
